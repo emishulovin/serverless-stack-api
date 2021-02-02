3d96ca7fd5960a0d72ee742070033c0a2545f521
Eliezer Mishulovin <50752841+emishulovin@users.noreply.github.com>
2021-02-01 18:54:52 -0600

    made helper functions for create

--- notes-api/create.js
+++ notes-api/create.js
@@ -1,12 +1,49 @@
+// original code without the libs folder.
+// import * as uuid from "uuid";
+// import AWS from "aws-sdk";
+
+// const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+// export async function main(event, context) {
+//   // Request body is passed in as a JSON encoded string in 'event.body'
+//   const data = JSON.parse(event.body);
+
+//   const params = {
+//     TableName: process.env.tableName,
+//     Item: {
+//       // The attributes of the item to be created
+//       userId: "123", // The id of the author
+//       noteId: uuid.v1(), // A unique uuid
+//       content: data.content, // Parsed from request body
+//       attachment: data.attachment, // Parsed from request body
+//       createdAt: Date.now(), // Current Unix timestamp
+//     },
+//   };
+
+//   try {
+//     await dynamoDb.put(params).promise();
+
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify(params.Item),
+//     };
+//   } catch (e) {
+//     console.log(e)
+//     return {
+
+//       statusCode: 500,
+
+//       body: JSON.stringify({ error: e.message }),
+//     };
+//   }
+// }
+
 import * as uuid from "uuid";
-import AWS from "aws-sdk";
+import handler from "./libs/handler-lib";
+import dynamoDb from "./libs/dynamodb-lib";
 
-const dynamoDb = new AWS.DynamoDB.DocumentClient();
-
-export async function main(event, context) {
-  // Request body is passed in as a JSON encoded string in 'event.body'
+export const main = handler(async (event, context) => {
   const data = JSON.parse(event.body);
-
   const params = {
     TableName: process.env.tableName,
     Item: {
@@ -19,18 +56,7 @@ export async function main(event, context) {
     },
   };
 
-  try {
-    await dynamoDb.put(params).promise();
+  await dynamoDb.put(params);
 
-    return {
-      statusCode: 200,
-      body: JSON.stringify(params.Item),
-    };
-  } catch (e) {
-    console.log(e);
-    return {
-      statusCode: 500,
-      body: JSON.stringify({ error: e.message }),
-    };
-  }
-}
+  return params.Item;
+});
